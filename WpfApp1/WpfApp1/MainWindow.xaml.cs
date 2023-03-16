157370c72c3393b65aaa4d189b06b573dba13db2
alowM228 <113525946+alowM228@users.noreply.github.com>
2023-03-16 16:28:11 +0300

    Add files via upload

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.sq1DataSetTableAdapters;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        group_TableAdapter sq1 =  new group_TableAdapter();
+        studentTableAdapter sq2 = new studentTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Aqw.ItemsSource = sq1.GetData();
+
+            ComboBox1.ItemsSource = sq2.GetData();
+            ComboBox1.DisplayMemberPath = "FIO";
+
+        }
+
+        private void ComboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            object cell = (ComboBox1.SelectedItem as DataRowView).Row [0];
+            MessageBox.Show(cell.ToString());
+        }
+    }
+}
